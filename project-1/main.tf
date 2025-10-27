5b774846e70c6d16eb6adfa19490567da7b44f49
learningdevops77 <learningdevops534@gmail.com>
2025-10-27 23:59:41 +0530

    Update main.tf

--- project-1/main.tf
+++ project-1/main.tf
@@ -30,7 +30,7 @@ resource "aws_subnet" "subnet_1" {
 
 resource "aws_subnet" "subnet_2" {
     vpc_id = aws_vpc.my-vpc.id
-    cidr_block = "10.1.0.0/24"
+    cidr_block = "10.0.2.0/24"
     availability_zone = "ap-south-1b"
     map_public_ip_on_launch = "true" 
     
